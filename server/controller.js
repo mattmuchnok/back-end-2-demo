34989530a422cb49d0fe232d0479b4dda1b689f0
mattmuchnok <mattmuchnok@gmail.com>
2022-02-08 14:23:32 -0500

    final commit

---
+++ server/controller.js
@@ -0,0 +1,44 @@
+const movies = require(`./db.json`)
+let globalID = 11
+
+module.exports = {
+    getAllMovies: (req,res) => {
+        res.status(200).send(movies)
+    },
+    deleteMovie: (req, res) => {
+        let index = movies.findIndex(elem => elem.id === +req.params.id)
+        movies.splice(index.toExponential, 1)
+        res.status(200).send(movies)
+    },
+    createMovie: (req, res) => {
+        let {title, rating, imageURL} = req.body
+        let newMovie = {
+            id: globalID,
+            title,
+            rating,
+            imageURL
+        }
+        movies.push(newMovie)
+        res.status(200).send(movies)
+        globalID++
+    },
+    updateMovie: (req, res) => {
+        let {id} = req.params
+        let {type} = req.body
+        let index = movies.findIndex(elem => elem.id === +id)
+
+        if (movies[index].rating === 5 && type === `plus`) {
+            res.status(400).send(`We cannot go above 5`)
+        } else if (movies[index].rating === 0 && type === `minus`) {
+            res.status(400).send(`We cannot go below 0`)
+        } else if (type === `plus`) {
+            movies[index].rating++
+            res.status(200).send(movies)
+        } else if (type === `minus`) {
+            movies[index].rating--
+            res.status(200).send(movies)
+        } else {
+            res.status(400).send(`You broke something!`)
+        }
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
 const express = require(`express`)
+const cors = require(`cors`)
+const ctrl = require(`./controller`)
+
+const app = express()
+
+const port = 4004
+
+app.use(express.json())
+app.use(cors())
+
+// Endpoints
+app.get(`/api/movies`, ctrl.getAllMovies)
+app.delete(`/api/movies/:id`, ctrl.deleteMovie)
+app.post(`/api/movies`, ctrl.createMovie)
+app.put(`/api/movies/:id`, ctrl.updateMovie)
+
+app.listen(port, () => console.log(`Avengers Assemble... on port ${port}`))
